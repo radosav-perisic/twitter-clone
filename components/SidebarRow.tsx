3eb58b1981cb945e2d903668c867313f49f24ce0
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-28 14:04:42 +0200

    Hide SidebarRow icon title on small screens

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -10,7 +10,7 @@ function SidebarRow ({Icon, title}: Props) {
   return (
     <div className='group flex max-w-fit items-center px-4 py-3 space-x-2 rounded-full hover:bg-gray-200 transition-all duration-200'>
         <Icon className='h-6 w-6'/>
-        <p className='group-hover:text-twitter'>{title}</p>
+        <p className='hidden md:inline-flex group-hover:text-twitter'>{title}</p>
     </div>
   )
 }
