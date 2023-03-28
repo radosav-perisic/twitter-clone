c6f92c7899d88b768bddc13f916af8d044c874d3
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-28 14:44:28 +0200

    Adjusted tweet box layout and added flex container

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -9,18 +9,18 @@ import {
 
 const Tweetbox = () => {
   return (
-    <div>
+    <div className="flex space-x-2 p-5">
       <img
         className="mt-4 h-14 w-14 rounded-full object-cover"
         src="https://links.papareact.com/gll"
         alt="/"
       />
 
-      <div>
-        <form>
-          <input type="text" placeholder="What's happening?" />
-          <div>
-            <div className="flex space-x-2 text-twitter">
+      <div className="flex flex-1 items-center pl-2">
+        <form className="flex flex-1 flex-col">
+          <input className="h-24 w-full text-xl outline-none placeholder:text-xl" type="text" placeholder="What's happening?" />
+          <div className="flex items-center">
+            <div className="flex space-x-2 text-twitter flex-1">
               {/* Icons */}
               <PhotographIcon className="h-5 w-5" />
               <SearchCircleIcon className="h-5 w-5" />
