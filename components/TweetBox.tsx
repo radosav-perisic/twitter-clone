45beb0c3924150952cafa897a9cde727e3e21069
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-28 14:52:42 +0200

    Changed tweetbox input to use controlled state

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   SearchCircleIcon,
   EmojiHappyIcon,
@@ -8,6 +8,8 @@ import {
 } from "@heroicons/react/outline";
 
 const Tweetbox = () => {
+   const [input, setInput] = useState<string>('')
+
   return (
     <div className="flex space-x-2 p-5">
       <img
@@ -18,15 +20,15 @@ const Tweetbox = () => {
 
       <div className="flex flex-1 items-center pl-2">
         <form className="flex flex-1 flex-col">
-          <input className="h-24 w-full text-xl outline-none placeholder:text-xl" type="text" placeholder="What's happening?" />
+          <input value={input} onChange={(e) =>setInput(e.target.value)} className="h-24 w-full text-xl outline-none placeholder:text-xl" type="text" placeholder="What's happening?" />
           <div className="flex items-center">
             <div className="flex space-x-2 text-twitter flex-1">
               {/* Icons */}
-              <PhotographIcon className="h-5 w-5" />
-              <SearchCircleIcon className="h-5 w-5" />
-              <EmojiHappyIcon className="h-5 w-5" />
-              <CalendarIcon className="h-5 w-5" />
-              <LocationMarkerIcon className="h-5 w-5" />
+              <PhotographIcon className="h-5 w-5 hover:cursor-pointer transition-transform duration-150 ease out hover:scale-150" />
+              <SearchCircleIcon className="h-5 w-5 hover:cursor-pointer transition-transform duration-150 ease out hover:scale-150" />
+              <EmojiHappyIcon className="h-5 w-5 hover:cursor-pointer transition-transform duration-150 ease out hover:scale-150" />
+              <CalendarIcon className="h-5 w-5 hover:cursor-pointer transition-transform duration-150 ease out hover:scale-150" />
+              <LocationMarkerIcon className="h-5 w-5 hover:cursor-pointer transition-transform duration-150 ease out hover:scale-150" />
             </div>
             <button className="bg-twitter px-5 py-2 font-bold text-white rounded-full">Tweet</button>
           </div>
