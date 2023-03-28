e2f9b1c95d70fc9808dc62f781bc1a1cc3b6eccc
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-28 13:06:13 +0200

    Applied group hover styling to sidebar row title

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -8,9 +8,9 @@ interface Props {
 
 function SidebarRow ({Icon, title}: Props) {
   return (
-    <div className='flex items-center px-4 py-3 space-x-2 rounded-full hover:bg-gray-200 transition-all duration-200'>
+    <div className='group flex max-w-fit items-center px-4 py-3 space-x-2 rounded-full hover:bg-gray-200 transition-all duration-200'>
         <Icon className='h-6 w-6'/>
-        <p>{title}</p>
+        <p className='group-hover:text-twitter'>{title}</p>
     </div>
   )
 }
