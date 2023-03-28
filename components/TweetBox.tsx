b7bb8823c39b9053b163cb7fb8ba60b3852d56be
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-28 14:40:10 +0200

    Add icons and input field to Tweetbox component

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -1,30 +1,34 @@
 import React from "react";
-import { SearchCircleIcon, EmojiHappyIcon, CalendarIcon, LocationMarkerIcon, PhotographIcon } from "@heroicons/react/outline";
+import {
+  SearchCircleIcon,
+  EmojiHappyIcon,
+  CalendarIcon,
+  LocationMarkerIcon,
+  PhotographIcon,
+} from "@heroicons/react/outline";
 
 const Tweetbox = () => {
   return (
     <div>
       <img
-        className="h-14 w-14 rounded-full object-cover"
+        className="mt-4 h-14 w-14 rounded-full object-cover"
         src="https://links.papareact.com/gll"
-        alt=""
+        alt="/"
       />
 
       <div>
         <form>
           <input type="text" placeholder="What's happening?" />
           <div>
-            <div>
-                {/* Icons */}
-                <PhotographIcon className="h-5- w-5" />
-                <SearchCircleIcon className="h-5- w-5" />
-                <EmojiHappyIcon className="h-5- w-5" />
-                <CalendarIcon className="h-5- w-5" />
-                <LocationMarkerIcon className="h-5- w-5" />
+            <div className="flex space-x-2 text-twitter">
+              {/* Icons */}
+              <PhotographIcon className="h-5 w-5" />
+              <SearchCircleIcon className="h-5 w-5" />
+              <EmojiHappyIcon className="h-5 w-5" />
+              <CalendarIcon className="h-5 w-5" />
+              <LocationMarkerIcon className="h-5 w-5" />
             </div>
-            <button>
-                Tweet
-            </button>
+            <button>Tweet</button>
           </div>
         </form>
       </div>
