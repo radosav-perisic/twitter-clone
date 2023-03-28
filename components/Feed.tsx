f2f040ec13637a8416154f05ea152e86dbc4acf2
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-28 13:15:40 +0200

    Add Refresh icon to Feed component

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,8 +1,12 @@
+import { RefreshIcon } from '@heroicons/react/outline'
 import React from 'react'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <div>
+        <h1>Home</h1>
+    <RefreshIcon />
+    </div>
   )
 }
 
