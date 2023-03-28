111778bd62d0c49c92f3da4d01c41c07485b10de
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-28 14:05:51 +0200

    Add spacing to Sidebar Twitter bird img

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -17,7 +17,7 @@ const Sidebar = () => {
   return (
     <div className="col-span-2 flex flex-col items-center px-4 md:items-start">
       <img
-        className="h-10 w-10"
+        className="m-3 h-10 w-10"
         src="https://links.papareact.com/drq"
         alt="/"
       />
