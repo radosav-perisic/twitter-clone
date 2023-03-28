164b8e5f0c81d55fb3bd2f2fdf2d9971756b391d
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-28 13:20:35 +0200

    Improve feed layout and design

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Feed = () => {
   return (
-    <div>
+    <div className='flex items-center justify-between'>
         <h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
     <RefreshIcon className='h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out rotate-180 active:scale-125'/>
     </div>
