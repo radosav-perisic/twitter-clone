63d090dec22d975fd935114af3e47c7c26aa3a35
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-28 13:27:49 +0200

    Style search bar in Widgets

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -5,9 +5,9 @@ const Widgets = () => {
   return (
     <div>
         {/* Search */}
-        <div>
-            <SearchIcon className='h-5 w-5'/>
-            <input type='text' placeholder='Search Twitter'/>
+        <div className='flex items-center bg-gray-100 space-x-2 p-3 rounded-full'>
+            <SearchIcon className='h-5 w-5 text-gray-500'/>
+            <input type='text' placeholder='Search Twitter' className='bg-transparent flex-1 outline-none'/>
         </div>
     </div>
   )
