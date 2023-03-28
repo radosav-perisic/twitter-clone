c2e97fecba5af46f1431fbf17850a5569e56a752
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-28 12:59:10 +0200

    Update SidebarRow styling

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 function SidebarRow ({Icon, title}: Props) {
   return (
-    <div>
+    <div className='flex items-center px-4 py-3 space-x-2 rounded-full hover:bg-gray-200 transition-all duration-200'>
         <Icon className='h-6 w-6'/>
         <p>{title}</p>
     </div>
