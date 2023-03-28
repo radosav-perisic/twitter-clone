b081a49632b76691aa960bf3727d7f7ce982f22d
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-28 10:43:37 +0200

    Add Twitter bird image to Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+
+import {
+  BellIcon,
+  HastagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from "@heroicons/react/outline";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  return <div>
+    <img className='h-10 w-10' src="https://links.papareact.com/drq" alt="/"/>
+  </div>;
+};
 
-export default Sidebar
+export default Sidebar;
