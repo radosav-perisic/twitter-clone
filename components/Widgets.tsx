0edd3786c8c7db77bc7de633f7f1123dc6952c3d
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-28 13:24:26 +0200

    Add search bar with SearchIcon to Widgets

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,8 +1,15 @@
+import { SearchIcon } from '@heroicons/react/outline'
 import React from 'react'
 
 const Widgets = () => {
   return (
-    <div>Widgets</div>
+    <div>
+        {/* Search */}
+        <div>
+            <SearchIcon className='h-5 w-5'/>
+            <input type='text' placeholder='Search Twitter'/>
+        </div>
+    </div>
   )
 }
 
