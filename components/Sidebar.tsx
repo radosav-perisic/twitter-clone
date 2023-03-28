5512984f866146752b0d8b8e94f717379a044c29
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-28 13:12:50 +0200

    Add flex column layout to Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -15,7 +15,7 @@ import SidebarRow from "./SidebarRow";
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className="flex flex-col">
       <img
         className="h-10 w-10"
         src="https://links.papareact.com/drq"
