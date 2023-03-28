2a73bddc6eadc24b22e196561d023d14f43014e5
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-28 13:39:16 +0200

    Add Twitter timeline to Widgets component

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,16 +1,26 @@
-import { SearchIcon } from '@heroicons/react/outline'
-import React from 'react'
+import { SearchIcon } from "@heroicons/react/outline";
+import React from "react";
+import { TwitterTimelineEmbed } from "react-twitter-embed";
 
 const Widgets = () => {
   return (
-    <div className='mt-2 px-2'>
-        {/* Search */}
-        <div className='flex items-center bg-gray-100 space-x-2 p-3 rounded-full'>
-            <SearchIcon className='h-5 w-5 text-gray-500'/>
-            <input type='text' placeholder='Search Twitter' className='bg-transparent flex-1 outline-none'/>
-        </div>
+    <div className="mt-2 px-2">
+      {/* Search */}
+      <div className="flex items-center bg-gray-100 space-x-2 p-3 rounded-full">
+        <SearchIcon className="h-5 w-5 text-gray-500" />
+        <input
+          type="text"
+          placeholder="Search Twitter"
+          className="bg-transparent flex-1 outline-none"
+        />
+      </div>
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="elonmusk"
+        options={{ height: 1000 }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Widgets
+export default Widgets;
