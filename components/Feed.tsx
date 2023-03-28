1f617cfd18c45852d43c8aeb3881980353c3d8b2
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-28 13:56:56 +0200

    Update Feed component layout

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 
 const Feed = () => {
   return (
+    <div className='col-span-5 '>
     <div className='flex items-center justify-between'>
         <h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
-    <RefreshIcon className='h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out rotate-180 active:scale-125'/>
+    <RefreshIcon className='h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125'/>
+    </div>
     </div>
   )
 }
