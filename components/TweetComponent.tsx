cce8ea9717827b59d83e54811e74772a4c56c4b9
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-30 15:05:02 +0200

    Update TweetComponent to include action icons

--- components/TweetComponent.tsx
+++ components/TweetComponent.tsx
@@ -1,37 +1,69 @@
-import React from 'react'
-import { Tweet } from '../typings'
-import Timeago from 'react-timeago'
-
+import React from "react";
+import { Tweet } from "../typings";
+import Timeago from "react-timeago";
+import {
+  ChatAlt2Icon,
+  HeartIcon,
+  SwitchHorizontalIcon,
+  UploadIcon,
+} from "@heroicons/react/outline";
 
 interface Props {
-    tweet: Tweet
-  }
-
-function TweetComponent({tweet}: Props) {
-  return( 
-    <div className='flex flex-col space-x-3 border-y p-5 border-gray-200'>
-        <div className='flex space-x-3'>
-            <img className='h-10 w-10 rounded-full object-cover' src={tweet.profileImg} alt='' />
-            <div>
-              <div className='flex items-center space-x-1'>
-                <p className='mr-1 font-bold'>{tweet.username}</p>
-                <p className='hidden text-sm text-gray-500 sm:inline'>@{tweet.username.replace(/\s+/g, '').toLowerCase()}</p>
-
-                <Timeago 
-                className='text-sm text-gray-500'
-                date={tweet._createdAt}
-                />
-              </div>
-            
-            <p className='pt-1'>{tweet.text}</p>
-             
-             {tweet.image && (
-              <img src={tweet.image} alt='' className='m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-xl'/>
-             )}
-            </div>
-        </div>
-    </div>
-  )
+  tweet: Tweet;
 }
 
-export default TweetComponent; 
+function TweetComponent({ tweet }: Props) {
+  return (
+    <div className="flex flex-col space-x-3 border-y p-5 border-gray-200">
+      <div className="flex space-x-3">
+        <img
+          className="h-10 w-10 rounded-full object-cover"
+          src={tweet.profileImg}
+          alt=""
+        />
+        <div>
+          <div className="flex items-center space-x-1">
+            <p className="mr-1 font-bold">{tweet.username}</p>
+            <p className="hidden text-sm text-gray-500 sm:inline">
+              @{tweet.username.replace(/\s+/g, "").toLowerCase()}
+            </p>
+
+            <Timeago
+              className="text-sm text-gray-500"
+              date={tweet._createdAt}
+            />
+          </div>
+
+          <p className="pt-1">{tweet.text}</p>
+
+          {tweet.image && (
+            <img
+              src={tweet.image}
+              alt=""
+              className="m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-xl"
+            />
+          )}
+        </div>
+      </div>
+      <div>
+        <div>
+          <ChatAlt2Icon className="h-5 w-5" />
+        </div>
+
+        <div>
+          <SwitchHorizontalIcon className="h-5 w-5" />
+        </div>
+
+        <div>
+          <HeartIcon className="h-5 w-5" />
+        </div>
+
+        <div>
+          <UploadIcon className="h-5 w-5" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TweetComponent;
