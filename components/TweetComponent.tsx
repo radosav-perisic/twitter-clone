fd68751ac6109096ffac06bbed92b9a5fd6d08d6
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-30 14:00:56 +0200

    Update tweet display to include Timeago timestamp

--- components/TweetComponent.tsx
+++ components/TweetComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Tweet } from '../typings'
+import Timeago from 'react-timeago'
 
 
 interface Props {
@@ -10,12 +11,18 @@ function TweetComponent({tweet}: Props) {
   return( 
     <div>
         <div>
-            <img src={tweet.profileImg} alt='/' />
+            <img src={tweet.profileImg} alt='' />
             <div>
               <div>
                 <p>{tweet.username}</p>
                 <p>@{tweet.username.replace(/\s+/g, '').toLowerCase()}</p>
+
+                <Timeago 
+                className='text-sm text-gray-500'
+                date={tweet._createdAt}
+                />
               </div>
+
             </div>
         </div>
     </div>
