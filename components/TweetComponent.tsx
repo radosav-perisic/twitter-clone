35b7cc5cdcdc047f8fc76032f3cbed93b1d56e50
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-30 14:07:59 +0200

    Update styling for TweetComponent

--- components/TweetComponent.tsx
+++ components/TweetComponent.tsx
@@ -13,9 +13,9 @@ function TweetComponent({tweet}: Props) {
         <div>
             <img src={tweet.profileImg} alt='' />
             <div>
-              <div>
-                <p>{tweet.username}</p>
-                <p>@{tweet.username.replace(/\s+/g, '').toLowerCase()}</p>
+              <div className='flex items-center space-x-1'>
+                <p className='mr-1 font-bold'>{tweet.username}</p>
+                <p className='hidden text-sm text-gray-500 sm:inline'>@{tweet.username.replace(/\s+/g, '').toLowerCase()}</p>
 
                 <Timeago 
                 className='text-sm text-gray-500'
