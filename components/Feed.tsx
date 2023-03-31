4e7a9f39ca0410e807f0f5769a21e0f513449aa5
Radosav Perisic <radosavperisic00@gmail.com>
2023-03-31 10:51:43 +0200

    Implement Feed refresh button functionality

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,6 +1,7 @@
 import { RefreshIcon } from '@heroicons/react/outline'
-import React from 'react'
+import React, { useState } from 'react'
 import { Tweet } from '../typings'
+import { fetchTweets } from '../utlis/fetchTweets'
 import Tweetbox from './TweetBox'
 import TweetComponent from './TweetComponent'
 
@@ -8,14 +9,20 @@ interface Props {
   tweets: Tweet[]
 }
 
-const Feed = ({tweets}: Props) => {
+const Feed = ({tweets: tweetsProp}: Props) => {
+  const [tweets, setTweets] = useState<Tweet[]>(tweetsProp)
     console.log(tweets);
+
+    const handleRefresh = async () => {
+      const tweets = await fetchTweets();
+      setTweets(tweets)
+    }
      
   return (
     <div className='col-span-7 lg:col-span-5 border-x'>
     <div className='flex items-center justify-between'>
         <h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
-    <RefreshIcon className='h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125'/>
+    <RefreshIcon onClick={handleRefresh} className='h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125'/>
     </div>
 
     {/* Tweetbox */}
