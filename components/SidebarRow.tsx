d2121b5be5785a2e8df8e07ed3c4e7eb443fc97c
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-01 14:08:16 +0200

    Add onClick event to SidebarRow component

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -4,11 +4,12 @@ import React, { SVGProps } from 'react'
 interface Props {
     Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element
     title: string
+    onClick?: () => {}
 }
 
-function SidebarRow ({Icon, title}: Props) {
+function SidebarRow ({Icon, title, onClick}: Props) {
   return (
-    <div className='group flex max-w-fit items-center px-4 py-3 space-x-2 rounded-full hover:bg-gray-200 transition-all duration-200'>
+    <div onClick={() => onClick?.()} className='group flex max-w-fit items-center px-4 py-3 space-x-2 rounded-full hover:bg-gray-200 transition-all duration-200'>
         <Icon className='h-6 w-6'/>
         <p className='hidden md:inline-flex group-hover:text-twitter text-base font-light lg:text-xl'>{title}</p>
     </div>
