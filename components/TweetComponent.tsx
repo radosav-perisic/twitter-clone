f4b925e44a2364ec60eab5ebf6d9499cdd86e3c0
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-02 21:04:54 +0200

    Refactor TweetComponent to use CommentBody type

--- components/TweetComponent.tsx
+++ components/TweetComponent.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Tweet, Comment } from "../typings";
-import Timeago from "react-timeago";
+import { Tweet, Comment, CommentBody } from "../typings";
+import TimeAgo from "react-timeago";
 import {
   ChatAlt2Icon,
   HeartIcon,
@@ -9,17 +9,18 @@ import {
 } from "@heroicons/react/outline";
 import { fetchComments } from "../utlis/fetchComments";
 import { useSession } from "next-auth/react";
+import  toast  from "react-hot-toast";
 
 interface Props {
   tweet: Tweet;
 }
 
-function TweetComponent({ tweet }: Props) {
+function Tweet({ tweet }: Props) {
   const [comments, setComments] = useState<Comment[]>([]);
   const [commentsBoxIsVisible, setCommentsBoxIsVisible] =
     useState<boolean>(false);
   const [input, setInput] = useState<string>("");
-  const {data: session} = useSession()
+  const { data: session } = useSession();
 
   const refreshComments = async () => {
     const comments: Comment[] = await fetchComments(tweet._id);
@@ -30,28 +31,51 @@ function TweetComponent({ tweet }: Props) {
     refreshComments();
   }, []);
 
-  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
-   e.preventDefault()
-  }
+  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+ 
 
-  console.log(comments);
+ const toastComment = toast.loading('Posting Comment...')
+
+{/* Comment logic*/}
+
+const comment: CommentBody = {
+  comment: input,
+  tweetId: tweet._id,
+  username: session?.user?.name || 'Unknown User',
+  profileImg: session?.user?.image || 'https://links.papareact.com/gll',
+}
+const result = await fetch(`/api/addComment`, {
+  body: JSON.stringify(comment),
+  method: 'POST',
+})
+
+console.log('Success!', result)
+toast.success('Comment Posted!', {
+  id: toastComment,
+})
+
+setInput('')
+setCommentsBoxIsVisible(false)
+refreshComments()
+};
 
   return (
-    <div className="flex flex-col space-x-3 border-y p-5 border-gray-200">
+    <div key={tweet._id} className="flex flex-col space-x-3 border-y p-5 border-gray-200">
       <div className="flex space-x-3">
         <img
           className="h-10 w-10 rounded-full object-cover"
-          src={tweet.profileImg}
+          src={tweet.profileImg || 'https://links.papareact.com/gll'}
           alt=""
         />
         <div>
           <div className="flex items-center space-x-1">
             <p className="mr-1 font-bold">{tweet.username}</p>
             <p className="hidden text-sm text-gray-500 sm:inline">
-              @{tweet.username.replace(/\s+/g, "").toLowerCase()}
+              @{tweet.username.replace(/\s+/g, "").toLowerCase()} ·
             </p>
 
-            <Timeago
+            <TimeAgo
               className="text-sm text-gray-500"
               date={tweet._createdAt}
             />
@@ -63,14 +87,16 @@ function TweetComponent({ tweet }: Props) {
             <img
               src={tweet.image}
               alt=""
-              className="m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-xl"
+              className="m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm"
             />
           )}
         </div>
       </div>
       <div className="flex mt-5 justify-between">
         <div
-          onClick={() => session && setCommentsBoxIsVisible(!commentsBoxIsVisible)}
+          onClick={(e) =>
+            session && setCommentsBoxIsVisible(!commentsBoxIsVisible)
+          }
           className=" flex cursor-pointer items-center text-gray-400 space-x-3"
         >
           <ChatAlt2Icon className="h-5 w-5" />
@@ -87,13 +113,13 @@ function TweetComponent({ tweet }: Props) {
 
         <div className="flex cursor-pointer items-center text-gray-400  space-x-3">
           <UploadIcon className="h-5 w-5" />
-        </div> 
+        </div>
       </div>
 
       {/* Comments Logic */}
 
       {commentsBoxIsVisible && (
-        <form  onSubmit={submitHandler}  className="mt-3 flex space-x-3 ">
+        <form onSubmit={submitHandler} className="mt-3 flex space-x-3 ">
           <input
             value={input}
             onChange={(e) => setInput(e.target.value)}
@@ -101,7 +127,13 @@ function TweetComponent({ tweet }: Props) {
             type="text"
             placeholder="Write a comment..."
           />
-          <button disabled={!input}className="text-twitter disable:text-gray-200">Post</button>
+          <button
+            disabled={!input}
+            className="text-twitter disabled:text-gray-200"
+            type="submit"
+          >
+            Post
+          </button>
         </form>
       )}
 
@@ -119,10 +151,10 @@ function TweetComponent({ tweet }: Props) {
                 <div className="flex items-center space-x-1 ">
                   <p className="mr-1 font-bold">{comment.username}</p>
                   <p className="hidden  text-sm text-gray-500 lg:inline">
-                    {comment.username.replace(/\s+/g, "").toLowerCase()}·
+                    @{comment.username.replace(/\s+/g, "").toLowerCase()}·
                   </p>
 
-                  <Timeago
+                  <TimeAgo
                     className="text-sm text-gray-500"
                     date={comment._createdAt}
                   />
@@ -137,4 +169,4 @@ function TweetComponent({ tweet }: Props) {
   );
 }
 
-export default TweetComponent;
+export default Tweet;
