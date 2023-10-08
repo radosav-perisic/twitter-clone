a4d2314d91d67ae19960dbc9f08417d79039dff9
Radosav Perisic <radosavperisic00@gmail.com>
2023-10-08 18:38:16 +0200

    Update SignIn component

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 function SidebarRow ({Icon, title, onClick}: Props) {
   return (
-    <div onClick={() => onClick?.()} className='group flex max-w-fit items-center px-4 py-3 space-x-2 rounded-full hover:bg-twitter bg-blue-300 transition-all duration-200'>
+    <div onClick={() => onClick?.()} className='group flex max-w-fit items-center px-4 py-3 space-x-2 rounded-full hover:bg-twitter bg-blue-300 transition-all cursor-pointer duration-200'>
         <Icon className='h-6 w-6'/>
         <p className='hidden md:inline-flex text-white text-base font-normal  lg:text-xl'>{title}</p>
     </div>
